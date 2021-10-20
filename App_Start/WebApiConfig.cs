05cf81fadaa87aeb4f9e088e76c0871112d942f9
Ruslan Aliyev <ruslanaliyev1849@gmail.com>
2021-10-20 17:07:35 +0700

    cors

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace TravelBlog
 {
@@ -16,6 +17,9 @@ namespace TravelBlog
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var cors = new EnableCorsAttribute("*", "*", "*");//origins,headers,methods 
+            config.EnableCors(cors);
         }
     }
 }
